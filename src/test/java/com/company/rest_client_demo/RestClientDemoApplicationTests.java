542b4eb8fa033fc534f0935ae0a76cf9e5d36957
Xedice Abbasova <abbasovaxdc@gmail.com>
2024-07-17 17:37:33 +0400

    created

---
+++ src/test/java/com/company/rest_client_demo/RestClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rest_client_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
